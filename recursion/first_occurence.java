0b2ef9617bd7de5cb228c593b493ffa6038fa442
Puneetrkrajput <79908438+Puneetrkrajput@users.noreply.github.com>
2022-11-20 14:43:13 +0530

    Add files via upload

---
+++ recursion/first_occurence.java
@@ -0,0 +1,19 @@
+public class first_occurence {
+    public static int fistoccur(int arr[],int i, int key){
+            if(i==arr.length){
+                return -1;
+            }
+            if(arr[i]==key){
+                return i;
+            }
+            return fistoccur(arr, i+1, key);
+            
+    }
+    public static void main(String[] args) {
+      
+        int arr[]={2,4,6,3,2,6,7,3,};
+        int key=3;
+        int i=0;
+        System.out.println(fistoccur(arr,i,key));
+    }
+}
---
+++ recursion/last_occurence.java
@@ -0,0 +1,21 @@
+public class last_occurence {
+    public static int lastoccur(int arr[],int i, int key){
+      if(i==arr.length){
+        return -1;
+      }
+      int isfound=lastoccur(arr, i+1, key);
+      if(isfound== -1 && arr[i]==key ){
+        return i;
+      }
+      return isfound;
+
+
+}
+public static void main(String[] args) {
+  
+    int arr[]={2,4,6,3,2,6,7,3,4};
+    int key=3;
+    int i=0;
+    System.out.println(lastoccur(arr,i,key));
+}
+}
