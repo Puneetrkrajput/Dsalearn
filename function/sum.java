40b8f719527225948fdc640a95b0866d3cc76189
Puneet Kumar <Puneet.kumar_cs20@gla.ac.in>
2022-11-30 00:16:26 +0530

    push sum

---
+++ function/sum.java
@@ -0,0 +1,12 @@
+package function;
+
+public class sum {
+
+    public static int sum(int a,int b){
+     return a+b;
+    }
+    public static void main(String[] args) {
+        System.out.println(sum(2,3));
+    }
+    
+}
--- recursion/isArraySorted.java
+++ recursion/isArraySorted.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 public class isArraySorted {
 
 
@@ -17,23 +17,3 @@ public class isArraySorted {
 
     }
 }
-=======
-public class isArraySorted {
-
-
-    public static boolean issorted(int arr[], int i){
-        if(i==arr.length-1){
-            return true;
-        }
-        if(arr[i]>arr[i+1]){
-            return false;
-        }
-        return issorted(arr, i+1);
-    }
-    public static void main(String[] args) {
-        int arr[]={1,2,3,7,5,6}; 
-        System.out.println(issorted(arr, 0));
-
-    }
-}
->>>>>>> 0b2ef9617bd7de5cb228c593b493ffa6038fa442
